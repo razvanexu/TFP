9c1302483e2c53a2e68712acde13e2d6f82be8a4
Razvan Dumitrescu <razvandf@gmail.com>
2019-08-14 16:25:27 +0300

    Create Enter-Nums.cpp
    Primul exercitiu TFP

---
+++ Enter-Nums.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Aplicatia-3-Ex-1
+//
+//  Created by Razvan Dumitrescu on 13/08/19.
+//  Copyright © 2019 Razvan Dumitrescu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // insert code here...
+    int a, b;
+    
+    cin >> a >> b;
+    
+    cout << a<<b<<"\n"<<b<<"\n"<<a<<"&"<<b<<"\n";
+    
+    return 0;
+}
